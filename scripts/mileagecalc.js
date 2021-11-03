695a879967fd046fdb05b17c980334d6fffb9884
dupaxtax <tanner.dupaix@gmail.com>
2021-11-02 22:10:26 -0600

    Added js file for calculator, added daily textbox

---
+++ scripts/mileagecalc.js
@@ -0,0 +1,9 @@
+function calcDaily(){
+    var monthly = parseInt(document.getElementById("total-miles").value);
+    var numDays = parseInt(document.getElementById("num-days").valule);
+
+    console.log(monthly);
+    console.log(numDays);
+    daily = monthly / 10;
+    console.log(daily)
+}
